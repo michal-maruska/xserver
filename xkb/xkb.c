954ee32967dc88ff131b5fa025d11c91fba92fe0
Michal Maruska <mmaruska@gmail.com>
2013-12-15 19:07:59 +0100

    Attach the pipeline to the xkb code: Init & various protocol requests to configure the plugins.
    Don't mention ar_plugin in xkb.h .... who needs it (pipeline.c),
    defines it itself for now.

--- xkb/xkb.c
+++ xkb/xkb.c
@@ -7074,6 +7074,19 @@ ProcXkbDispatch(ClientPtr client)
         return ProcXkbSetDeviceInfo(client);
     case X_kbSetDebuggingFlags:
         return ProcXkbSetDebuggingFlags(client);
+#ifdef MMC_PIPELINE             /* XChangeDeviceProperty ? */
+    case X_kbSetPlugin:
+       return ProcXkbSetPlugin(client);
+    case X_kbPluginSetConfig:
+       return ProcXkbConfigPlugin(client, FALSE);
+    case X_kbPluginGetConfig:
+       return ProcXkbConfigPlugin(client, TRUE);
+    case X_kbPluginCommand:
+       return ProcXkbCommandPlugin(client);
+    case X_kbListPipeline:
+       return ProcXkbListPipeline(client);
+#endif /* MMC_PIPELINE */
+
     default:
         return BadRequest;
     }
@@ -7111,5 +7124,9 @@ XkbExtensionInit(void)
         XkbErrorBase = (unsigned char) extEntry->errorBase;
         XkbKeyboardErrorCode = XkbErrorBase + XkbKeyboard;
     }
+
+#if MMC_PIPELINE
+    pipeline_init_plugins();
+#endif
     return;
 }
