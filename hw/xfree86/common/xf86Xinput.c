9cfda2f38ae9b44800be2f809052235f03941ab4
Michal Maruska <mmaruska@gmail.com>
2014-09-18 00:55:02 +0200

    Extend the Xfree86 DDX api for drivers, to pass the timestamp of keyboard events. new ABI version.
    Provide the default timestamp -- could have called the non-extended api.
    Implementation "bridges" to the new extended DIX api.
    
    What's the point of xf86PostKeyboardTimeEvent vs xf86PostKeyEventMTime ?

--- hw/xfree86/common/xf86Xinput.c
+++ hw/xfree86/common/xf86Xinput.c
@@ -1434,6 +1434,12 @@ xf86PostKeyEventP(DeviceIntPtr device,
 void
 xf86PostKeyEventM(DeviceIntPtr device, unsigned int key_code, int is_down)
 {
+    xf86PostKeyEventMTime(device, key_code, is_down, GetTimeInMillis());
+}
+
+void
+xf86PostKeyEventMTime(DeviceIntPtr device, unsigned int key_code, int is_down, Time time)
+{
 #ifdef XFreeXDGA
     DeviceIntPtr pointer;
 
@@ -1447,8 +1453,8 @@ xf86PostKeyEventM(DeviceIntPtr device, unsigned int key_code, int is_down)
             return;
     }
 #endif
-
-    QueueKeyboardEvents(device, is_down ? KeyPress : KeyRelease, key_code);
+    QueueKeyboardEventsTime(device,
+                            is_down ? KeyPress : KeyRelease, key_code, time);
 }
 
 void
@@ -1459,6 +1465,15 @@ xf86PostKeyboardEvent(DeviceIntPtr device, unsigned int key_code, int is_down)
     valuator_mask_zero(&mask);
     xf86PostKeyEventM(device, key_code, is_down);
 }
+
+/* New entry point for keyboard drivers, which provide the event's timestamp. */
+/* fixme: drop this, useless. */
+void
+xf86PostKeyboardTimeEvent(DeviceIntPtr device, unsigned int key_code, int is_down, Time time)
+{
+    xf86PostKeyEventMTime(device, key_code, is_down, time);
+}
+
 
 InputInfoPtr
 xf86FirstLocalDevice(void)
