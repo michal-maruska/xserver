6f7a7e8ea3d9328a54d964aa5e51e03bdda391ab
Michal Maruska <mmaruska@gmail.com>
2014-09-28 14:56:29 +0200

    Do update the non-event current time.

--- dix/dispatch.c
+++ dix/dispatch.c
@@ -195,6 +195,7 @@ GetCurrentClient(void)
     return currentClient;
 }
 
+/* mmc: what is this HWEventQueue ? */
 void
 SetInputCheck(HWEventQueuePtr c0, HWEventQueuePtr c1)
 {
@@ -202,6 +203,8 @@ SetInputCheck(HWEventQueuePtr c0, HWEventQueuePtr c1)
     checkForInput[1] = c1;
 }
 
+/* updates the notion of time, independent of events!!!
+ * Just from the clock! */
 void
 UpdateCurrentTime(void)
 {
@@ -232,21 +235,25 @@ UpdateCurrentTime(void)
 }
 
 /* Like UpdateCurrentTime, but can't call ProcessInputEvents */
+/* mmc: update currentTime, handling wrap-over.  */
 void
 UpdateCurrentTimeIf(void)
 {
-#if mmc_debug
     TimeStamp systime;
     systime.months = currentTime.months;
     systime.milliseconds = GetTimeInMillis();
     if (systime.milliseconds < currentTime.milliseconds)
         systime.months++;
     if (CompareTimeStamps(systime, currentTime) == LATER) {
+#if mmc_debug
         ErrorF("%s: %u->%u\n", __FUNCTION__, currentTime.milliseconds,
                systime.milliseconds);
-	// currentTime = systime;
-    }
 #endif
+        /* I wanted to keep the time of last event processed.
+         * Because events can come from `past' which is bad.
+         * So basically this is per-device. Should be  InputDeviceTime..... */
+	currentTime = systime;
+    }
 }
 
 #undef SMART_DEBUG
