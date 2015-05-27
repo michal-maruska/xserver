8882bf07604e07c4d8f8dabfe41668f1719f4e04
Michal Maruska <mmaruska@gmail.com>
2015-05-27 09:40:28 +0200

    trace ComputeFreezes conditionally.

--- dix/events.c
+++ dix/events.c
@@ -1352,9 +1352,17 @@ ComputeFreezes(void)
 #endif
         )
       {
+#if DEBUG_MMC || 1
+          ErrorF("%s%s%s: detected recursive call-> return.\n", event_color,
+                 __FUNCTION__, color_reset);
+#endif
           return;
       }
 
+#if DEBUG_MMC
+    ErrorF("%s%s%s: now scanning and releasing events!\n",
+           event_color, __FUNCTION__, color_reset);
+#endif
     syncEvents.playingEvents = TRUE;
     /* mmc: this is only for replaying the 1 event which caused Grab */
     if (replayDev) {
@@ -1395,7 +1403,10 @@ ComputeFreezes(void)
     /* mmc: find the first/_any_ device not frozen ...
      * fixme: This should run only on devs which were frozen up to now!   */
     for (dev = devices; dev; dev = dev->next) {
-            if (!dev->deviceGrab.sync.frozen) {
+#if (DEBUG_MMC && 0)
+        ErrorF("\t%s\n", dev->name);
+#endif
+        if (!dev->deviceGrab.sync.frozen) {
 #if MMC_PIPELINE
             if (dev->public.thawProc) {
                 dev->public.thawProc(dev);
