e94f3dc5d3a013cfb5e45db759377c41a1048dfb
Michal Maruska <mmaruska@gmail.com>
2018-05-28 12:28:12 +0200

    try to fix the crash

--- xkb/xkb.c
+++ xkb/xkb.c
@@ -2800,7 +2800,8 @@ ProcXkbSetMap(ClientPtr client)
     if (rc != Success)
         return rc;
 
-    master = GetMaster(dev, MASTER_KEYBOARD);
+    // mmc: maybe fails?
+    // master = GetMaster(dev, MASTER_KEYBOARD);
 
     if (stuff->deviceSpec == XkbUseCoreKbd) {
         DeviceIntPtr other;
@@ -2818,6 +2819,8 @@ ProcXkbSetMap(ClientPtr client)
             }
         }
     } else {
+#if 0
+        // mmc:  so this was added.
         DeviceIntPtr other;
 
         for (other = inputInfo.devices; other; other = other->next) {
@@ -2829,6 +2832,7 @@ ProcXkbSetMap(ClientPtr client)
             if (rc != Success)
                 return rc;
         }
+#endif
     }
 
     /* We know now that we will succeed with the SetMap. In theory anyway. */
@@ -2853,6 +2857,7 @@ ProcXkbSetMap(ClientPtr client)
             }
         }
     } else {
+#if 0
         DeviceIntPtr other;
 
         for (other = inputInfo.devices; other; other = other->next) {
@@ -2862,6 +2867,7 @@ ProcXkbSetMap(ClientPtr client)
 
             _XkbSetMap(client, other, stuff, tmp); //ignore rc
         }
+#endif
     }
 
     return Success;
