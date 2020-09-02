650ad9002ea179cbcedf29ab5d761d4a6f3b63ea
Michal Maruska <mmaruska@gmail.com>
2020-09-02 13:04:59 +0200

    Manage the device-specific queues.
    Create a per-device mieq queue. DIX does registration on Enable (not on add INputDevice).
    mieq_close_device_queue to drop the per-device event-queue when the dix input device is disabled
    New tool function: find_queue.
    
    This is after return from VT.

--- mi/mi.h
+++ mi/mi.h
@@ -183,6 +183,9 @@ extern _X_EXPORT void miStepDash(int /*dist */ ,
 typedef struct _DeviceRec *DevicePtr;
 #endif
 
+extern _X_EXPORT Bool mieq_init_device_queue(DeviceIntPtr);
+extern _X_EXPORT void mieq_close_device_queue (DeviceIntPtr dev);
+
 extern _X_EXPORT Bool mieqInit(void
     );
 
