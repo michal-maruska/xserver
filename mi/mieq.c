4547645e70a8fb82e4c975ec18e8fa6799beecef
Michal Maruska <mmaruska@gmail.com>
2022-02-13 19:44:40 +0100

    [bad half!] mieqProcessInputEventsTime: push only up to the max time.
    Event processing is interspersed with this pushing?

--- mi/mieq.c
+++ mi/mieq.c
@@ -640,19 +640,6 @@ mieqProcessDeviceEvent(DeviceIntPtr dev, InternalEvent *event, ScreenPtr screen)
     }
 }
 
-/* Call this from ProcessInputEvents(). */
-void
-mieqProcessInputEventsTime(Time now)
-{
-    mieqProcessInputEvents();
-#if MMC_PIPELINE
-    if (now)
-    {
-        push_time_to_devices(now);
-    }
-#endif
-}
-
 #if USE_SEPARATE_QUEUES
 static
 void push_time_to_devices(Time time)
@@ -792,13 +779,21 @@ void miManageQueue(EventQueuePtr eq)
 void
 mieqProcessInputEvents(void)
 {
+    mieqProcessInputEventsTime(0);
+}
+
+void
+mieqProcessInputEventsTime(Time time_max)
+{
     EventRec *e = NULL;
     ScreenPtr screen;
     static InternalEvent event; /* mmc: optimization? */
-    DeviceIntPtr dev = NULL, master = NULL;
+    DeviceIntPtr dev = NULL;
     static Bool inProcessInputEvents = FALSE;
 #if USE_SEPARATE_QUEUES
     int index;
+    Bool still_pushing_time = TRUE;
+    Time pushed_time = 0;
 #endif
 
     input_lock();
@@ -828,8 +823,6 @@ mieqProcessInputEvents(void)
      *  device[i] -> first y of yyyy
      * here I would take the first of ND number-of-devices.
      * */
-    Bool still_pushing_time = TRUE;
-    Time pushed_time = 0;
     while ((index = find_first_non_empty(&e)) != -1)
     {
         EventQueuePtr eq;
@@ -852,15 +845,18 @@ mieqProcessInputEvents(void)
         // Imagine the last read (b/c of select) is from mouse,
         // and it deliveres `last_minute' events.
         // Cannot be applied to other devices!
-        ErrorF("maybe push time %lu\n", event.any.time);
-        if (still_pushing_time
-            && (time_max > event.any.time)) {
-            if (pushed_time < time_max) {
-                push_time_to_devices(time_max);
+        if (still_pushing_time) {
+            if (time_max > event.any.time) {
+                pushed_time = event.any.time;
+                push_time_to_devices(pushed_time);
+            } else {
+                still_pushing_time = FALSE;
+                /* last time: */
+                if (pushed_time < time_max) {
+                    pushed_time = time_max;
+                    push_time_to_devices(pushed_time);
+                }
             }
-            still_pushing_time = FALSE;
-        } else {
-            push_time_to_devices(event.any.time);
         }
         // push the event.
         push_event_to_device(dev, &event, screen);
@@ -887,6 +883,14 @@ mieqProcessInputEvents(void)
 
 #endif // USE_SEPARATE_QUEUES
     inProcessInputEvents = FALSE;
+
+/* fixme: is this not necessary for !USE_SEPARATE_QUEUES ? */
+#if USE_SEPARATE_QUEUES
+    if (pushed_time < time_max) {
+        pushed_time = time_max;
+        push_time_to_devices(pushed_time);
+    }
+#endif // USE_SEPARATE_QUEUES
 
     CallCallbacks(&miCallbacksWhenDrained, NULL);
 
