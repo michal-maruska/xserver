4ad791086cd3c0d6e789332b94d89efdaf7d781f
Michal Maruska <mmaruska@gmail.com>
2022-02-18 21:04:19 +0100

    re-enable the "master" keyboard?

--- xkb/xkb.c
+++ xkb/xkb.c
@@ -2811,7 +2811,7 @@ ProcXkbSetMap(ClientPtr client)
         return rc;
 
     // mmc: maybe fails?
-    // master = GetMaster(dev, MASTER_KEYBOARD);
+    master = GetMaster(dev, MASTER_KEYBOARD);
 
     if (stuff->deviceSpec == XkbUseCoreKbd) {
         DeviceIntPtr other;
@@ -2829,7 +2829,7 @@ ProcXkbSetMap(ClientPtr client)
             }
         }
     } else {
-#if 0
+#if 1
         // mmc:  so this was added.
         DeviceIntPtr other;
 
